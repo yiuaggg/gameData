028b2319c02437d31ad802f9a9fdaf137e03fe33
肖垚 <1902719129@qq.com>
2024-03-20 11:35:06 +0800

    feat: 增加接口

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ SECRET_KEY = "DnKRYZbvVzdhPlF01jmjxmi5Cj36AbCd"
 # redis 配置
 REDIS_HOST = '127.0.0.1'
 REDIS_PORT = '6379'
-REDIS_PASSWORD = ''
+REDIS_PASSWORD = 'rediscaicai'
 
 # api token 配置
 API_EXPIRATION = 3600
